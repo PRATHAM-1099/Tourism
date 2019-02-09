cfaf208464c64b3ba5fa1143f4c8f5f722f65f87
PRATHAMESH <prathameshagurav@gmail.com>
2019-02-09 13:22:31 +0000

    new

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://prathamesh:yash@1202@ds127545.mlab.com:27545/yelpcamp");
+mongoose.connect("mongodb://prathamesh:iampratham99@ds127545.mlab.com:27545/yelpcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
