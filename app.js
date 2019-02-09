373f06b67f40c9704672821b51a7b965e13b3c93
PRATHAMESH <prathameshagurav@gmail.com>
2019-02-09 12:32:38 +0000

    app added

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://localhost/yelp_camp_v10");
+mongoose.connect("mongodb://pratham10:yash@123@ds127545.mlab.com:27545/yelpcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
